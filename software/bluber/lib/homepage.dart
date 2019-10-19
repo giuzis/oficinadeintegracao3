1bd2c7055ca6b731443084451a2a0eff4bee91ce
marcellepires <mreispires@hotmail.com>
2019-10-19 14:27:14 -0300

    Retirando QRCode Scanner

--- software/bluber/lib/homepage.dart
+++ software/bluber/lib/homepage.dart
@@ -3,10 +3,7 @@
     _googleMap1()
     _googleMap2()
 */
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:barcode_scan/barcode_scan.dart';
-import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
@@ -60,39 +57,12 @@ class _MyHomePageState extends State<MyHomePage>
           icon: Icon(Icons.directions_bike),
           label: Text('Quero pedalar!'),
           onPressed: () {
-            scan('/emviagem');
+            //scan('/emviagem');
           },
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
     );
-  }
-
-  //Funcao utilizada para scannear o QrCode
-  Future scan(String page) async {
-    try {
-      String barcode = await BarcodeScanner.scan();
-      setState(() {
-        this._barcode = barcode;
-        // vai apra a outra página
-        // aqui vai ter que ter alguma coisa para conectar o celular e a bike via bluetooth
-        // para ver se ela está disponível e se o código confere
-        Navigator.of(context).pushReplacementNamed(page);
-      });
-      print(_barcode);
-    } on PlatformException catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        setState(() {
-          this._barcode = 'E';
-        });
-      } else {
-        setState(() => this._barcode = 'E');
-      }
-    } on FormatException {
-      setState(() => this._barcode = 'E');
-    } catch (e) {
-      setState(() => this._barcode = 'E');
-    }
   }
 
   // widget que define o banner do drawer
