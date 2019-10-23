b1619633baca3a482e86facc4757d602f84ef3f4
mglucas <mglucas@debian>
2019-10-23 00:38:56 -0300

    gsm 100% waiting integration

--- hardware/gsmModule/gsmModule.ino
+++ hardware/gsmModule/gsmModule.ino
@@ -9,71 +9,12 @@ void setup()
 {
   //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
-  
   //Begin serial communication with Arduino and SIM800L
   mySerial.begin(9600);
-
-  Serial.println("Initializing..."); 
-  delay(1000);
-  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
-  updateSerial();
-  delay(1000);
-  mySerial.println("ATE1&W"); //Once the handshake test is successful, it will back to OK
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+HTTPINIT"); // Configuring TEXT mode
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+HTTPSSL=1"); // Configuring TEXT mode
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CGATT?"); // Configuring TEXT mode
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CIPMUX=0"); // Configuring TEXT mode
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CFUN=1"); // Enable All
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CPIN?");
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CSTT=\"timbrasil.br\", \"tim\", \"tim\"");
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CIICR");
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CIFSR");
-  updateSerial();
-  delay(1000);
-  mySerial.println("AT+CIPSTART=\"TCP\",\"bluberstg.firebaseio.com\",80");
-  updateSerial();
-  delay(3000);
-  mySerial.println("AT+CIPSEND=66");
-  updateSerial();
-  delay(1000);
-  int teste = mySerial.println(
-    "GET /users/alarm.json HTTP/1.1\r\n"
-    "Host: bluberstg.firebaseio.com\r\n");
-  Serial.println(teste);
-  updateSerial(); 
-  delay(5000);
-  updateSerial();
-  delay(5000);
-  mySerial.println("AT+CIPSHUT");
-  updateSerial();
 }
 
 void loop()
 {
-  updateSerial();
-}
-
-void updateSerial()
-{
-  delay(500);
   while (Serial.available()) 
   {
     mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
