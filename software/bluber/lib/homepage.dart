69fed9636e0e30fd07fb500b7be7d9fbcc24de05
marcellepires <mreispires@hotmail.com>
2019-11-03 20:05:13 -0300

    Alterações na localização de início do mapa

--- software/bluber/lib/homepage.dart
+++ software/bluber/lib/homepage.dart
@@ -38,6 +38,8 @@ class _MyHomePageState extends State<MyHomePage>
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
   MarkerId selectedMarker;
   Location location = Location();
+
+  // var myLocation = new Location();
   // int _markerIdCounter = 1;
   static final LatLng center = const LatLng(-25.438376, -49.263781);
   BitmapDescriptor myIcon;
@@ -175,10 +177,12 @@ class _MyHomePageState extends State<MyHomePage>
       getInformation(email);
     }
 
-    // if (!bikesUpdated) {
+    var pos = location.getLocation();
+
+    if (!bikesUpdated) {
     //Pega as bicicletas ativas
-    adicicionaBikes();
-    //   }
+      adicicionaBikes();
+    }
 
     // clearList().then((value){
     pegaHistoPessoais(email);
@@ -201,8 +205,8 @@ class _MyHomePageState extends State<MyHomePage>
             _drawerBanner(),
             _drawerList(),
             Padding(
-              //padding: EdgeInsets.only(top: 150),
-              padding: EdgeInsets.only(top: 370),
+              padding: EdgeInsets.only(top: 150),
+              // padding: EdgeInsets.only(top: 370),
               child: _signOutButton(),
             )
           ],
@@ -394,10 +398,11 @@ class _MyHomePageState extends State<MyHomePage>
   // modificar essas duas funções para incluir o mapa
   // tutorial que pode ajudar https://www.youtube.com/watch?v=lNqEfnnmoHk&t=347s
   Widget _googleMap1(BuildContext context) {
+    
     return GoogleMap(
       mapType: MapType.normal,
       initialCameraPosition: CameraPosition(
-        target: LatLng(-25.438376, -49.263781),
+        target: LatLng(-25.4391239,-49.2688287),
         zoom: 15,
       ),
       onMapCreated: _onMapCreated,
@@ -827,33 +832,35 @@ class _MyHomePageState extends State<MyHomePage>
         // print(numCorridas);
 
         lista_historico_corridas.clear();
-        for (int i = 0; i < numCorridas; i++) {
-          String corridaAtual = _corrida[i].toString().replaceAll(" ", "");
-          print(corridaAtual.toString());
-          String bike_id = corridas[corridaAtual]['bike_id'] as String;
-          String cliente = corridas[_corrida[i]]['cliente'] as String;
-          String data_e_hora_fim =
-              corridas[_corrida[i]]['data_e_hora_fim'] as String;
-          String data_e_hora_inicio =
-              corridas[_corrida[i]]['data_e_hora_inicio'] as String;
-          String preco = corridas[_corrida[i]]['preco'] as String;
-          String photoName = _corrida[i].toString();
+        if(numCorridas < 1){
+            for (int i = 0; i < numCorridas; i++) {
+              String corridaAtual = _corrida[i].toString().replaceAll(" ", "");
+              print(corridaAtual.toString());
+              String bike_id = corridas[corridaAtual]['bike_id'] as String;
+              String cliente = corridas[_corrida[i]]['cliente'] as String;
+              String data_e_hora_fim =
+                  corridas[_corrida[i]]['data_e_hora_fim'] as String;
+              String data_e_hora_inicio =
+                  corridas[_corrida[i]]['data_e_hora_inicio'] as String;
+              String preco = corridas[_corrida[i]]['preco'] as String;
+              String photoName = _corrida[i].toString();
 
-          Viagem viagemNova = new Viagem(bike_id, cliente, data_e_hora_fim,
-              data_e_hora_inicio, preco, photoName);
+              Viagem viagemNova = new Viagem(bike_id, cliente, data_e_hora_fim,
+                  data_e_hora_inicio, preco, photoName);
 
-          // if(!lista_historico_corridas.contains(viagemNova)){
-          lista_historico_corridas.add(viagemNova);
-          // }
-        }
+              // if(!lista_historico_corridas.contains(viagemNova)){
+              lista_historico_corridas.add(viagemNova);
+              // }
+            }
 
-        print(lista_historico_corridas.length.toString());
-      } else {
-        if (response.statusCode == 201) {
-          // print("Histórico vazio");
-        } else {
-          // msgErroViagem();
-        }
+            print(lista_historico_corridas.length.toString());
+          } else {
+            if (response.statusCode == 201) {
+              // print("Histórico vazio");
+            } else {
+              // msgErroViagem();
+            }
+          }
       }
     });
   }
